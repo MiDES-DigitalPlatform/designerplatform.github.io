4ea7090a46c8d392012c9966ec837d82eca388d7
POUCHENTW\shawn.huang <shawn.huang@pouchen.com>
2024-10-01 09:55:53 +0800

    Add Desigher platform web resources.

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "MiDES-DesignerKit-0.1.0";
+const contentToCache = [
+    "Build/bb0d9ecdb05db3e84da20bd14a4f84dc.loader.js",
+    "Build/598fd2062c5172aeef96e3d8d3b1e29a.framework.js",
+    "Build/086f420f31bbfc315664cb6b712c1975.data",
+    "Build/e560b1a7850ce795124e2c7420c1df64.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
