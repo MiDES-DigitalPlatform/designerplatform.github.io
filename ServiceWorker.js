8df26e1eb5af2fb54d275e391d3c36fcbf6e9d6f
yam2560981 <yam2560981@gmail.com>
2024-10-18 08:46:29 +0800

    模型Read/Write修改

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -2,7 +2,7 @@ const cacheName = "MiDES-DesignerKit-0.1.0";
 const contentToCache = [
     "Build/bb0d9ecdb05db3e84da20bd14a4f84dc.loader.js",
     "Build/fb39452ab62fb1f99169d5ea20addb4c.framework.js",
-    "Build/aa653a04457542e595df136316f826f3.data",
+    "Build/1b6060d9296fa975dd38e1bdcf7b1e09.data",
     "Build/e1b51d2740c8c4e2d00f9a4cdb9c4ce6.wasm",
     "TemplateData/style.css"
 
