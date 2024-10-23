18ee02abd8bed6b746c8e6dc9fcbfaf07b6cb7f1
yam2560981 <yam2560981@gmail.com>
2024-10-23 14:41:37 +0800

    ATD59-ATD77

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "MiDES-DesignerKit-0.1.0";
 const contentToCache = [
     "Build/bb0d9ecdb05db3e84da20bd14a4f84dc.loader.js",
-    "Build/be98bd9e1f50f3001d86bc4c7b2cf9b9.framework.js",
-    "Build/18eb3d17a227d7dbe0be38ce39a12fa0.data",
-    "Build/247e04e9b4fe7f1e3aaaf3054644e31e.wasm",
+    "Build/b7de5e37247e05d02d5a2e66f6ac7567.framework.js",
+    "Build/31ae7f2ab39a14100a1183eac66dde0d.data",
+    "Build/4d49deb1e66ddab6a90380551e40f580.wasm",
     "TemplateData/style.css"
 
 ];
