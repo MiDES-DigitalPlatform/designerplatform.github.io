316ca3b790f888682f82eeadbbc0a04702a1ad14
yam2560981 <yam2560981@gmail.com>
2024-10-25 17:27:17 +0800

    滾輪前後更改 uv更改

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "MiDES-DesignerKit-0.1.0";
 const contentToCache = [
     "Build/bb0d9ecdb05db3e84da20bd14a4f84dc.loader.js",
-    "Build/e2b42a2b8576da19d539aba78c7bed4a.framework.js",
-    "Build/f615196b4bdd01966662cfeb7cb06408.data",
-    "Build/8922844a103c4078f1df9ee35aeddf2c.wasm",
+    "Build/6da6446d90c050c1834e3e852e54a734.framework.js",
+    "Build/d36b11f4461d00db062d943f1f713005.data",
+    "Build/bde4b2814ae68b3162d64e2b32cf7a1b.wasm",
     "TemplateData/style.css"
 
 ];
