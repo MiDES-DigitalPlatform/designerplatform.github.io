7e49cce9694cb7f1f9ab90a892f6b433839352ee
yam2560981 <yam2560981@gmail.com>
2024-11-04 15:55:34 +0800

    no message

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "MiDES-DesignerKit-0.1.0";
 const contentToCache = [
     "Build/bb0d9ecdb05db3e84da20bd14a4f84dc.loader.js",
-    "Build/01549ed8e3ae2c607f710127eae1d74d.framework.js",
-    "Build/a99500d1dcc4a1b527bbe37ae4264a87.data",
-    "Build/a937f88626f0be95469730ecc97417eb.wasm",
+    "Build/e7713fee0d577d610a8c81022347b43b.framework.js",
+    "Build/74fb53dbd49b06de26b32809e127577c.data",
+    "Build/7be6793016097c7e7b306393b2df1d84.wasm",
     "TemplateData/style.css"
 
 ];
